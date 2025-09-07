76e27832f44cdb26dc19ec7a2c1e4a8a51bae044
Kunj Maheshwari <kunjm@outlook.com>
2025-09-07 14:58:57 +0530

    Maximum Subarray Sum

---
+++ ContiguousSubarray.java
@@ -0,0 +1,17 @@
+class Solution{
+    public static int kadansAlgorithm(int nums[]){
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            currentSum += nums[i];
+
+            maxSum = currentSum > maxSum ? currentSum : maxSum;
+
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
